507a6afd087d42bf7e7eb2712305559b5379ffe3
Jaap Joris Vens <jj@rtts.eu>
2016-05-18 15:44:32 +0200

    python packaging with setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os, sys
+from setuptools import setup, find_packages
+
+setup(
+    name = 'django-pandocfield',
+    version = '0.1',
+    url = 'https://github.com/JaapJoris/django-pandocfield',
+    author = 'Jaap Joris Vens',
+    author_email = 'jj@rtts.eu',
+    license = 'GPL3',
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [
+        'django >= 1.7.7, <= 1.9.4',
+    ],
+)

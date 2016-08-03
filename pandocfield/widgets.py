f76ee02b8b6366f7d8616225e693e85295efc448
Jaap Joris Vens <jj@rtts.eu>
2016-08-03 16:02:49 +0200

    Improved the looks of the editor widget

--- pandocfield/widgets.py
+++ pandocfield/widgets.py
@@ -1,10 +1,23 @@
 from django import forms
+from django.template import loader
+from django.forms.utils import flatatt
+from django.utils.html import format_html
+from django.utils.encoding import force_text
 
 class PandocEditor(forms.widgets.Textarea):
     """Form widget specifically for editing PandocFields
 
     """
-    def render(self, name, value, attrs=None):
-        if hasattr(value, 'raw'):
+    def render(self, name, value, attrs={}):
+        if value is None:
+            value = ''
+        elif hasattr(value, 'raw'):
             value = value.raw
-        return super(PandocEditor, self).render(name, value, attrs)
+        attrs['style'] = 'font-family: monospace; line-height: 1.5; height: 25em; width: calc(100% - 190px) !important'
+        final_attrs = self.build_attrs(attrs, name=name)
+        print(final_attrs)
+        template = loader.get_template('pandocfield/widget.html')
+        return template.render({
+            'attrs': flatatt(final_attrs),
+            'value': force_text(value),
+        })
